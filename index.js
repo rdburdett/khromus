5ec8401f5257c20e4971f9d5c2eb622ba28e40f9
Robert Burdett <46675270+rdburdett@users.noreply.github.com>
2024-02-12 20:58:10 -0600

    improve error handling

--- index.js
+++ index.js
@@ -1,38 +1,53 @@
 import chalk from "chalk";
 
 export default function log(...args) {
-    args.forEach((data) => {
-        let logColor;
-        let type;
-        switch (typeof data) {
-            case "string":
-                type = "String";
-                logColor = "green";
-                break;
-            case "object":
-                type = Array.isArray(data) ? "Array" : "Object";
-                logColor = Array.isArray(data) ? "cyan" : "blue";
-                if (Array.isArray(data)) {
-                    data = `[${data.join(",")}]`; // Join array values with a comma
-                } else {
-                    data = JSON.stringify(data, null, 2); // Properly format objects
-                }
-                break;
-            case "number":
-                type = "Number";
-                logColor = "yellow";
-                break;
-            case "boolean":
-                type = "Boolean";
-                logColor = "magenta";
-                break;
-            default:
-                type = "Undefined/Error";
-                logColor = "red";
-                break;
-        }
-        console.log(chalk[logColor](`${type}: \n${data}\n`));
-    });
+	try {
+		args.forEach((data) => {
+			let logColor;
+			let type;
+			let logtype = true;
+			switch (typeof data) {
+				case "string":
+					type = "String";
+					logColor = "green";
+					break;
+				case "object":
+					type = Array.isArray(data) ? "Array" : "Object";
+					logColor = Array.isArray(data) ? "cyan" : "blue";
+					data = Array.isArray(data)
+						? `[${data.join(",")}]`
+						: JSON.stringify(data, null, 2);
+					break;
+				case "number":
+					type = "Number";
+					logColor = "yellow";
+					break;
+				case "function":
+					type = "Function";
+					logColor = "gray";
+					data = data.toString();
+					break;
+				case "boolean":
+					type = "Boolean";
+					logColor = "magenta";
+					break;
+				case "undefined":
+					type = "Undefined";
+					logColor = "red";
+					break;
+				default:
+					throw new Error("Unsupported data type");
+			}
+			console.log(chalk[logColor](`${type}: ${data}`));
+		});
+	} catch (error) {
+		console.error(chalk.red(`Error logging data: ${error.message}`));
+		console.error(chalk.red("Data causing error:", data));
+	}
 }
 
-log("Test Output.", [1, 2, 3], 42, true, undefined);
+log("Test Output.", [1, 2, 3], 42, true, undefined, new Date(), function () {
+	console.log("Hello!");
+});
+
+// Todo: Create ability to toggle display of log type
